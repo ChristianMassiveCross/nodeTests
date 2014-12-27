4f5402185f8d3265027cbacd442a96716524b3b4
christian <christian@minimi.com>
2014-12-27 13:00:18 +0100

    add new view to index.js create this new view in /view

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -5,5 +5,9 @@ var router = express.Router();
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
+/* GET home page. */
+router.get('/helloworld', function(req, res) {
+  res.render('hello', { title: 'hallo title' });
+});
 
 module.exports = router;
